d68093759bc4a269da1f768ef242115e64d62978
pinky160388 <155429522+pinky160388@users.noreply.github.com>
2026-05-28 17:15:12 +0200

    Update test_rebalance.py

--- tests/test_rebalance.py
+++ tests/test_rebalance.py
@@ -2,6 +2,7 @@ from src.rebalance import Security, rebalance_portfolio
 
 
 def test_ibm_should_buy():
+
     portfolio = [
         Security("IBM", 20, 10, 150)
     ]
@@ -9,9 +10,11 @@ def test_ibm_should_buy():
     result = rebalance_portfolio(portfolio)
 
     assert result["IBM"]["action"] == "BUY"
+    assert result["IBM"]["shares"] == 67
 
 
 def test_orcl_should_sell():
+
     portfolio = [
         Security("ORCL", 20, 30, 220)
     ]
@@ -19,9 +22,11 @@ def test_orcl_should_sell():
     result = rebalance_portfolio(portfolio)
 
     assert result["ORCL"]["action"] == "SELL"
+    assert result["ORCL"]["shares"] == 45
 
 
 def test_msft_should_hold():
+
     portfolio = [
         Security("MSFT", 20, 20, 90)
     ]
@@ -29,13 +34,4 @@ def test_msft_should_hold():
     result = rebalance_portfolio(portfolio)
 
     assert result["MSFT"]["action"] == "HOLD"
-
-
-def test_aapl_should_hold():
-    portfolio = [
-        Security("AAPL", 20, 20, 450)
-    ]
-
-    result = rebalance_portfolio(portfolio)
-
-    assert result["AAPL"]["action"] == "HOLD"
+    assert result["MSFT"]["shares"] == 0
